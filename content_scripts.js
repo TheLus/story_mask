47be84cfd854607930cde26dcb64f9cf536a2e05
Masahiro Nakahashi <bori42195@gmail.com>
2021-02-07 12:47:26 +0900

    微調整

--- content_scripts.js
+++ content_scripts.js
@@ -1,10 +1,23 @@
-const RIGHT_MERGINE = {
+const LEFT_MARGIN = {
+  normal: 0,
+  big: 0,
+  one: -2
+};
+const RIGHT_MARGIN = {
   normal: 3,
   big: 5,
-  one: 7
+  one: 9
 };
-const CHAR_MERGINE = {
-  f: { normal: 3, big: 4, one: 5 }
+const CHAR_LEFT_MARGIN = {
+  j: { normal: -5, big: -7, one: -12 },
+  p: { normal: -1, big: -1, one: -2 },
+  u: { normal: -1, big: -1, one: -2 }
+};
+const CHAR_RIGHT_MARGIN = {
+  f: { normal: 4, big: 4, one: 8 },
+  p: { normal: 0, big: 0, one: 3 },
+  r: { normal: 2, big: 0, one: 3 },
+  y: { normal: 2, big: 0, one: 2 }
 };
 const wordMask = {};
 new MutationObserver(records => {
@@ -25,12 +38,14 @@ const fetchMaskData = (sizeName) => {
   document.querySelectorAll('.word-for-mask').forEach((word, i) => {
     const inner = word.querySelector('.word-for-mask-inner');
     const innerText = inner.innerText;
+    const firstChar = innerText[0];
     const lastChar = innerText[innerText.length - 1];
-    const charMargine = CHAR_MERGINE[lastChar] ? CHAR_MERGINE[lastChar][sizeName] : 0;
+    const charLeftMargin = CHAR_LEFT_MARGIN[firstChar] ? CHAR_LEFT_MARGIN[firstChar][sizeName] : 0;
+    const charRightMargin = CHAR_RIGHT_MARGIN[lastChar] ? CHAR_RIGHT_MARGIN[lastChar][sizeName] : 0;
     words[i] = {
       text: word.innerText,
-      left: word.offsetLeft + inner.offsetLeft,
-      width: inner.offsetWidth + RIGHT_MERGINE[sizeName] + charMargine
+      left: word.parentElement.offsetLeft + word.offsetLeft + inner.offsetLeft + LEFT_MARGIN[sizeName] + charLeftMargin,
+      width: inner.offsetWidth + RIGHT_MARGIN[sizeName] + charRightMargin - charLeftMargin // 左にずらした分幅が足りなくなるので - charLeftMargin して足してあげる
     };
   });
   wordMask[sizeName] = words;
